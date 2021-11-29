9a427b584811fb6a3542bc77e694379c95d5c724
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-11-29 20:46:46 +0200

    login tested and working

--- login.php
+++ login.php
@@ -2,5 +2,37 @@
 
 include("connection.php");
 
+if (isset($_POST["email"]) && ($_POST["email"] != "")) {
+    $email = $_POST["email"];
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
 
+if (isset($_POST["password"]) && ($_POST["password"] != "")) {
+    $password = hash("sha256", $_POST["password"]);
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
+
+$query = "SELECT * FROM users WHERE email=? AND password=?";
+// $query = "SELECT * FROM users";
+$stmt = $connection->prepare($query);
+$stmt->bind_param("ss",$email,$password);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$temp_array = [];
+
+while ($row = $result->fetch_assoc()) {
+    $temp_array[] = $row;
+}
+
+$json = json_encode($temp_array);
+print $json;
+
+$stmt->close();
+$connection->close();
+    //REDIRECT
 ?>
