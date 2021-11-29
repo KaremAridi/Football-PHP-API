640b0a51fb68191b8d86a481541dd75101fb5d75
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-11-29 16:20:47 +0200

    signup page done

--- signup.php
+++ signup.php
@@ -1,5 +1,37 @@
 <?php
 
+include("connection.php");
 
+if (isset($_POST["first_name"]) && ($_POST["first_name"] != "")) {
+    $first_name = $_POST["first_name"];
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
 
+if (isset($_POST["last_name"]) && ($_POST["last_name"] != "")) {
+    $last_name = $_POST["last_name"];
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
+
+if (isset($_POST["email"]) && ($_POST["email"] != "")) {
+    $email = $_POST["email"];
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
+
+if (isset($_POST["password"]) && ($_POST["password"] != "") && ($_POST["password"] == $_POST["password_confirm"])) {
+    $password = hash("sha256", $_POST["password"]);
+} else {
+    die("Passwords did not match");
+}
+
+$mysql = $connection->prepare("INSERT INTO users(first_name, last_name, email, password) VALUES(?,?,?,?)");
+
+$mysql->bind_param("ssss", $first_name, $last_name, $email, $password); //since all are string ssss
+$mysql->execute();
+
+$mysql->close();
+$connection->close();
+//REDIRECT
 ?>
