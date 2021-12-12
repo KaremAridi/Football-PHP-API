5c752f684f40c26d92935a3810f1e477351c9427
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-12-12 23:01:34 +0200

    change api to liketeam

--- likesTeam.php
+++ likesTeam.php
@@ -1,15 +1,31 @@
 <?php
+
 include("connection.php");
 
-$user_id = $data->user_id;
-$team_id = $data->team_id;
+if (isset($_GET["user_id"]) && ($_GET["user_id"] != "")) {
+    $user_id = $_GET["user_id"];
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
 
-$mysql = $connection->prepare("INSERT INTO likes_teams(user_id,team_id) VALUES(?,?)");
+if (isset($_GET["team_id"]) && ($_GET["team_id"] != "")) {
+    $team_id = $_GET["team_id"];
+} else {
+    die("We took your IP address and the FBI is on his way");
+}
 
-$mysql->bind_param("ii", $user_id, $team_id);
-$mysql->execute();
+$query = "INSERT INTO likes_teams(user_id,team_id) VALUES(?,?)";
 
-$mysql->close();
+$stmt = $connection->prepare($query);
+$stmt->bind_param("ss",$user_id,$team_id);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$json = json_encode($result);
+
+$stmt->close();
 $connection->close();
-//REDIRECT
+    //REDIRECT
 ?>
