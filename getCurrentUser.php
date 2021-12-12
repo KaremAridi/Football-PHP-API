8c72cbdf46c1cf2a3ebde131f20f1e95adea4965
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-12-12 20:13:22 +0200

    adding for storage

--- getCurrentUser.php
+++ getCurrentUser.php
@@ -2,23 +2,9 @@
 
 include("connection.php");
 
-if (isset($_GET["email"]) && ($_GET["email"] != "")) {
-    $email = $_GET["email"];
-} else {
-    die("We took your IP address and the FBI is on his way");
-}
-
-if (isset($_GET["password"]) && ($_GET["password"] != "")) {
-    $password = hash("sha256", $_GET["password"]);
-} else {
-    die("We took your IP address and the FBI is on his way");
-}
-
 $query = "SELECT * FROM logged_in JOIN users ON logged_in.user_id = logged_in.id AND user_id=1;";
 
 $stmt = $connection->prepare($query);
-$stmt->bind_param("ss",$email,$password);
-
 $stmt->execute();
 
 $result = $stmt->get_result();
